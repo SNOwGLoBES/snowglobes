a6cc19a569a2af1ff3461eed1260e6a8101c5163
Jost Migenda <jost.migenda@kcl.ac.uk>
2023-02-21 19:10:51 +0000

    Add __init__.py files for Python 3.9 compatibility

--- snowglobes_data/setup.sh
+++ snowglobes_data/setup.sh
@@ -4,6 +4,10 @@
 mkdir -p src/snowglobes_data
 cp -r ../LICENSE ../detector_configurations.dat ../channels ../effic ../smear ../xscns src/snowglobes_data/
 
+# Create __init__ files for submodules. Without these, code like `files(snowglobes_data.channels)` raises an error in importlib.resources under Python 3.9
+# See https://setuptools.pypa.io/en/latest/userguide/datafiles.html#accessing-data-files-at-runtime
+touch src/snowglobes_data/__init__.py src/snowglobes_data/channels/__init__.py src/snowglobes_data/effic/__init__.py src/snowglobes_data/smear/__init__.py src/snowglobes_data/xscns/__init__.py
+
 # Build package using configurations in pyproject.toml
 pip install --upgrade build
 python -m build
