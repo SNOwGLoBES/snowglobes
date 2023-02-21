0be33c417bc31e07d835476b549b68c3c48bfaa0
Jost Migenda <jost.migenda@kcl.ac.uk>
2023-02-21 19:16:39 +0000

    Get version automatically from git tag

--- snowglobes_data/setup.sh
+++ snowglobes_data/setup.sh
@@ -4,6 +4,10 @@
 mkdir -p src/snowglobes_data
 cp -r ../LICENSE ../detector_configurations.dat ../channels ../effic ../smear ../xscns src/snowglobes_data/
 
+# Get version number from latest git tag
+VERSION=`git describe --abbrev=0 | sed 's/v//'`
+echo "__version__ = '$VERSION'" > src/snowglobes_data/__init__.py
+
 # Create __init__ files for submodules. Without these, code like `files(snowglobes_data.channels)` raises an error in importlib.resources under Python 3.9
 # See https://setuptools.pypa.io/en/latest/userguide/datafiles.html#accessing-data-files-at-runtime
 touch src/snowglobes_data/__init__.py src/snowglobes_data/channels/__init__.py src/snowglobes_data/effic/__init__.py src/snowglobes_data/smear/__init__.py src/snowglobes_data/xscns/__init__.py
