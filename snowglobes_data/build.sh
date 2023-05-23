d6203045c791f541d29f95a53dfcf01bd7f47497
Jost Migenda <jost.migenda@kcl.ac.uk>
2023-05-23 18:47:37 +0100

    improved publishing workflow and build script

--- snowglobes_data/build.sh
+++ snowglobes_data/build.sh
@@ -4,9 +4,11 @@
 mkdir -p src/snowglobes_data
 cp -r ../LICENSE ../detector_configurations.dat ../channels ../effic ../smear ../xscns src/snowglobes_data/
 
-# Get version number from latest git tag
-VERSION=`git describe --abbrev=0 | sed 's/v//'`
-echo "__version__ = '$VERSION'" > src/snowglobes_data/__init__.py
+# Get version number from environment variable (in GitHub Action); fall back to latest git tag for local installs
+if [ ! $GIT_VERSION ]; then
+  GIT_VERSION=`git describe --dirty | sed 's/v//'`;
+fi
+echo "__version__ = '$GIT_VERSION'" > src/snowglobes_data/__init__.py
 
 # Create __init__ files for submodules. Without these, code like `files(snowglobes_data.channels)` raises an error in importlib.resources under Python 3.9
 # See https://setuptools.pypa.io/en/latest/userguide/datafiles.html#accessing-data-files-at-runtime
